ed5a5346d79846503e1b13f07b0188f024bf9db4
Adam Cuppy <adam@codingzeal.com>
2014-12-15 21:59:27 -0800

    simplify specs

--- lib/trueman.rb
+++ lib/trueman.rb
@@ -1,7 +1,7 @@
 require "trueman/version"
 
 module Trueman
-  TRUE_VALUES  = [true, 1, '1', 't', 'T', 'true', 'TRUE']
+  TRUE_VALUES  = [true,  1, '1', 't', 'T', 'true',  'TRUE']
   FALSE_VALUES = [false, 0, '0', 'f', 'F', 'false', 'FALSE']
 
   def self.truthy?(value)
--- spec/trueman_spec.rb
+++ spec/trueman_spec.rb
@@ -10,7 +10,6 @@ describe Trueman do
 
     it "allows a new value" do
       expect(Trueman.truthy?('foo')).to eq true
-      expect(Trueman.truthy?('bar')).not_to eq true
     end
   end
 
@@ -23,66 +22,18 @@ describe Trueman do
   end
 
   describe 'determine a falsy value' do
-    it 'returns true with the false object' do
-      expect(Trueman.falsy?(false)).to eq true
-    end
-
-    it 'returns true with the integer 0' do
-      expect(Trueman.falsy?(0)).to eq true
-    end
-
-    it 'returns true with the string "0"' do
-      expect(Trueman.falsy?("0")).to eq true
-    end
-
-    it 'returns true with the string "false"' do
-      expect(Trueman.falsy?("false")).to eq true
-    end
-
-    it 'returns true with the string "FALSE"' do
-        expect(Trueman.falsy?("FALSE")).to eq true
-    end
-
-    it 'returns true with the string "f"' do
-      expect(Trueman.falsy?("f")).to eq true
-    end
-
-    it 'returns true with the string "F"' do
-      expect(Trueman.falsy?("F")).to eq true
+    [0, false, "false", "FALSE", "f", "F"].each do |value|
+      it "returns true for the value #{value}" do
+        expect(Trueman.falsy?(value)).to eq true
+      end
     end
   end
 
   describe 'determine a truthy value' do
-    it 'returns true with the true object' do
-      expect(Trueman.truthy?(true)).to eq true
-    end
-
-    it 'returns true with the integer 1' do
-      expect(Trueman.truthy?(1)).to eq true
-    end
-
-    it 'returns true with the string "1"' do
-      expect(Trueman.truthy?("1")).to eq true
-    end
-
-    it 'returns true with the string "true"' do
-      expect(Trueman.truthy?("true")).to eq true
-    end
-
-    it 'returns true with the string "TRUE"' do
-      expect(Trueman.truthy?("TRUE")).to eq true
-    end
-
-    it 'returns true with the string "t"' do
-      expect(Trueman.truthy?("t")).to eq true
-    end
-
-    it 'returns true with the string "T"' do
-      expect(Trueman.truthy?("T")).to eq true
-    end
-
-    it 'returns false with a false value' do
-      expect(Trueman.truthy?(false)).to eq false
+    [1, true, "true", "TRUE", "t", "T"].each do |value|
+      it "returns true for the value #{value}" do
+        expect(Trueman.truthy?(value)).to eq true
+      end
     end
   end
 end
